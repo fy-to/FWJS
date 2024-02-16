912424894c61cf7146ed9a28e63b92a602e7e16c
Gasquez Florian <m@fy.to>
2024-02-16 15:16:56 +0100

    -

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -2,14 +2,36 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
+import replace from "@rollup/plugin-replace";
+
+const replaceOptions = {
+  __DATE__: new Date().toISOString(),
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {},
+        transformAssetUrls: {
+          video: ["src", "poster"],
+          source: ["src"],
+          img: ["src"],
+          Image: ["src"],
+          Video: ["src"],
+          image: ["xlink:href", "href"],
+          use: ["xlink:href", "href"],
+        },
+      },
+    }),
+    // @ts-ignore
+    replace(replaceOptions),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "~@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
