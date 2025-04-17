7b0b8ff17c0a1aac99cfe7d4f565b9f7becd0d83
Gasquez Florian <m@fy.to>
2025-04-17 23:33:05 +0200

    fx

--- packages/fws-vue/src/components/ui/DefaultModal.vue
+++ packages/fws-vue/src/components/ui/DefaultModal.vue
@@ -243,10 +243,10 @@ const handleBackdropClick = useDebounceFn((event: MouseEvent) => {
 <template>
   <ClientOnly>
     <transition
-      enter-active-class="duration-300 ease-out"
+      enter-active-class="duration-50 ease-out"
       enter-from-class="opacity-0"
       enter-to-class="opacity-100"
-      leave-active-class="duration-200 ease-in"
+      leave-active-class="duration-50 ease-in"
       leave-from-class="opacity-100"
       leave-to-class="opacity-0"
     >
